0f8e0a528dcb005acc00b37c70f4ba1cb11daed4
xuyanghe <tracerxu@126.com>
2023-10-19 22:48:51 +0800

    Site updated: 2023-10-19 22:48:25

---
+++ 2021/06/24/deb与rpm打包/源码包/debbuild/ctest-1.0/CTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "It works" << std::endl;
+
+	std::string str;
+
+	std::cin >> str;
+
+    return 0;
+}
---
+++ 2021/06/24/deb与rpm打包/源码包/debbuild/test-1.0/debian/test.doc-base.EX
@@ -0,0 +1,20 @@
+Document: test
+Title: Debian test Manual
+Author: <insert document author here>
+Abstract: This manual describes what test is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/test/test.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/test/test.ps.gz
+
+Format: text
+Files: /usr/share/doc/test/test.text.gz
+
+Format: HTML
+Index: /usr/share/doc/test/html/index.html
+Files: /usr/share/doc/test/html/*.html
